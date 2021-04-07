f87331e106add1cdeedf69055d8ae7f81bacaaef
Resilient923 <kc960923@naver.com>
2021-04-07 15:13:47 +0900

    [update] react 210407

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
 import { Component } from 'react';
-import './components/TOC';
-import './components/Article';
-import './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 
 
 class App extends Component { //컴포넌트 만드는 코드
-  render(){
+  constructor(props) {//렌더보다 먼저실행되면서 초기화 시켜주고싶은 코드는 constructor 안에씀
+    super(props);
+    this.state = {//state의값 초기화
+      mode : 'welcome',
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome:{title:'Welcome!', desc:'Hello, React!'},
+      contetns:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render() {
+    var _title, _desc = null;
+    if(this.state.mode == 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode == 'read'){
+      _title = this.state.contetns[0].title;
+      _desc = this.state.contetns[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"> </Subject>
-        <TOC></TOC>
-        <Article></Article>
+        <Subject title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contetns}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,12 +1,17 @@
-import React,{ Component } from 'react';
-class Subject extends Component { //컴포넌트 만드는 코드
+import React, { Component } from 'react';
+class Subject extends Component {
     render(){//render 라고 하는 메소드가 있다. function 생략 class안의 함수는 render
+     
       return (
         <header>
-        <h1>WEB</h1>
-        world wide web!
-    </header>  
+            <h1><a href="/"></a>{this.props.title}</h1>
+            {this.props.sub}
+        </header>  
       );
     }
   }
-  export default Subject;
+
+export default Subject; 
+      
+  
+     
