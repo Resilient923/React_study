c93f91a5fb47e7779cf1d62e51fd06f0282895be
Resilient923 <kc960923@naver.com>
2021-04-09 01:43:54 +0900

    [update] App.js 210409

--- src/App.js
+++ src/App.js
@@ -21,18 +21,28 @@ class App extends Component { //컴포넌트 만드는 코드
   }
   render() {
     var _title, _desc = null;
-    if(this.state.mode == 'welcome'){
+    if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    }else if(this.state.mode == 'read'){
+    }else if(this.state.mode === 'read'){
       _title = this.state.contetns[0].title;
       _desc = this.state.contetns[0].desc;
     }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title}
+        {/* <Subject title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+          <h1>
+            <a href="/" onClick={function(e){//event 생성해주기
+            //hi 알림창 누름면 새로고침되는현상 발생 default값이 event로 들어감
+              alert('hi');
+              e.preventDefault();// default event를 막아준다.
+            }}>{this.state.subject.title}</a>
+            {this.state.subject.sub}
+          </h1>
+        </header>
         <TOC data={this.state.contetns}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
