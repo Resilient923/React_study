976d3b99fa6b395ef5f0b47e33869cd6d07e458d
Resilient923 <kc960923@naver.com>
2021-04-11 22:07:07 +0900

    [update] App 210411

--- src/App.js
+++ src/App.js
@@ -1,96 +1,137 @@
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import ReadContent from "./components/ReadContent"
+import CreateContent from "./components/CreateContent"
+import UpdateContent from "./components/UpdateContent"
+import Subject from "./components/Subject"
+import Control from "./components/Control"
 import './App.css';
-import { Component } from 'react';
-import TOC from './components/TOC';
-import ReadContent from './components/ReadContent';
-import Subject from './components/Subject';
-import Control from './components/Control';
-import CreateContent from './components/CreateContent';
 
 
-
-class App extends Component { //APP 이라는 컴포넌트 만드는 코드
-  constructor(props) {//렌더보다 먼저실행되면서 초기화 시켜주고싶은 코드는 constructor 안에씀
+class App extends Component {
+  constructor(props){
     super(props);
     this.max_content_id = 3;
-    this.state = {//state의값 초기화
-      mode: 'create',
-      selected_content_id :2,
-      subject: { title: 'WEB', sub: 'World Wide Web!' },
-      welcome: { title: 'Welcome!', desc: 'Hello, React!' },
-      contents: [
-        { id: 1, title: 'HTML', desc: 'HTML is for information' },
-        { id: 2, title: 'CSS', desc: 'CSS is for design' },
-        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
+    this.state = {
+      mode:'welcome',
+      selected_content_id:2,
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
       ]
     }
   }
-  render() { 
-    var _title, _desc,_article = null;
-    if (this.state.mode === 'welcome') {//Article 영역이 교체되는 코드
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if (this.state.mode === 'read') {
-      var i = 0;
+  getReadContent(){
+    var i = 0;
       while(i < this.state.contents.length){
         var data = this.state.contents[i];
         if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
+          return data;
           break;
         }
         i = i + 1;
       }
+  }
+  getContent(){
+    var _title, _desc, _article = null;
+    if(this.state.mode ===  'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    }else if(this.state.mode === 'create'){
-      _article = <CreateContent onSubmit={function(_title,_desc){
-        //add content to this.state.contents
-        this.max_content_id +=1;
-        //this.state.contents.push({id:this.max_content_id, title:_title, desc:_desc});
-        //push 함수는 원본을 바꾼다. 위에는 퍼포먼스를 수정할때 어렵다
-        // concat은 원본을 변경하는 함수
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        )
+    } else if(this.state.mode === 'read'){
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else  if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id+1;
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
-          console.log(_title,_desc)
       }.bind(this)}></CreateContent>
+    } else  if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          var _contents = Array.from(this.state.contents);//immutable
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
+        }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
+    console.log('App render');
     return (
       <div className="App">
-        {<Subject title={this.state.subject.title}//onChangePage라는 이벤트생성
+        <Subject 
+          title={this.state.subject.title} 
           sub={this.state.subject.sub}
-          onChangePage={function () {//만든이벤트
-            this.setState({mode:'change'});
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
           }.bind(this)}
         >
-        </Subject>}
+        </Subject>
         <TOC 
-        onChangePage={function(id){
-          this.setState({
-            mode:'read',
-            selected_content_id:Number(id)
-          });
-         }.bind(this)}
-          data={this.state.contents}></TOC>
-          <Control onChangeMode ={function(_mode){
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)} 
+          data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={function(_mode){
+          if(_mode === 'delete'){
+              if(window.confirm('really?')){
+                var _contents = Array.from(this.state.contents);
+                 var i = 0;
+                 while(i<this.state.contents.length){
+                   if(_contents[i].id===this.state.selected_content_id){
+                     _contents.splice(i,1);
+                     break;
+                   }
+                  i = i+1;
+                 }
+                 this.setState({
+                   mode:'welcome',
+                   contents : _contents
+                 });
+                 alert('delete!');
+              }
+          }else{
             this.setState({
               mode:_mode
             });
-          }.bind(this)}></Control>
-        {_article}
+          }
+          
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
   }
 }
- 
+
 export default App;
 
 
-
-{/* <header>
+/* {<header>
           <h1>
             <a href="/" onClick={function(e){//event 생성해주기
             //hi 알림창 누름면 새로고침되는현상 발생 default값이 event로 들어감
@@ -104,4 +145,4 @@ export default App;
             }.bind(this)}>{this.state.subject.title}</a>
             {this.state.subject.sub}
           </h1>
-        </header> */}
+        </header>} */
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,32 +1,33 @@
-import React,{ Component } from 'react';
-class TOC extends Component { //컴포넌트 만드는 코드
+import React, { Component } from 'react';
+
+class TOC extends Component{
     render(){
+      console.log('TOC render');
       var lists = [];
       var data = this.props.data;
       var i = 0;
       while(i < data.length){
         lists.push(
-        <li key={data[i].id}>
-          <a href={"/content/"+data[i].id}
-          data-id = {data[i].id}
-          onClick = {function(e){
-           // e.target 에러발생
-          e.preventDefault();
-            this.props.onChangePage(e.target.dataset.id);
-          }.bind(this)}
-          >{data[i].title}</a>
-        </li>);
-        i = i+1;
+          <li key={data[i].id}>
+            <a 
+              href={"/content/"+data[i].id}
+              data-id={data[i].id}
+              onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage(e.target.dataset.id);
+              }.bind(this)}
+            >{data[i].title}</a>
+          </li>);
+        i = i + 1;
       }
       return (
         <nav>
-              <ul>
-                  {lists}
-              </ul>
-          </nav>
+            <ul>
+                {lists}
+            </ul>
+        </nav>
       );
     }
   }
 
-  export default TOC;
-  
+export default TOC;
